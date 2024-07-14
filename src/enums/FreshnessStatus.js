f76f824f3863764d3940d21d7ca36012b2540903
Abhilash Mukherjee1 <Abhilash.Mukherjee1@CTIPL-MAC-083.local>
2024-07-14 15:33:09 +0530

    added freshness status enum

---
+++ src/enums/FreshnessStatus.js
@@ -0,0 +1,15 @@
+export const FreshnessStatus = Object.freeze({
+    Premature: {
+      description: "Not yet ripe",
+      color: "green.500"
+    },
+    Edible: {
+      description: "Ready to eat",
+      color: "yellow.500"
+    },
+    Rotten: {
+      description: "Spoiled",
+      color: "red.500"
+    }
+  });
+    
